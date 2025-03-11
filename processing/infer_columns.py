77cb82166ca3759ffb27a135da40b4823d53e690
Denise Müller <134210655+dzzling@users.noreply.github.com>
2025-03-11 15:01:42 +0100

    tweek topic modelling

--- processing/infer_columns.py
+++ processing/infer_columns.py
@@ -1,7 +1,9 @@
+# %%
 # Dependencies
 
 import polars as pl
 
+# %%
 # Read into dataframe
 
 df1 = pl.read_csv("./../../data/v2/30min_data.csv", ignore_errors=True)
@@ -9,14 +11,21 @@ df2 = pl.read_csv("./../../data/v2/240min_data.csv", ignore_errors=True)
 df3 = pl.read_csv("./../../data/v2/front_page_data.csv", ignore_errors=True)
 df4 = pl.read_csv("./../../data/v2/second_chance_pool.csv", ignore_errors=True)
 
+# %%
 # Infer weekday from time
 df1 = df1.with_columns(((((pl.col("time") - 32400) // 86400) + 4) % 7).alias("weekday"))
 
+# %%
 # Infer hour from time
 df1 = df1.with_columns(((pl.col("time") // 3600) % 24).alias("hour"))
 
+# %%
 # Infer title length
 df1 = df1.with_columns(pl.col("title").str.len_chars().alias("title_length"))
 
+# %%
 # Infer body length
 df1 = df1.with_columns(pl.col("text").str.len_chars().alias("body_length"))
+
+# %%
+# Infer topic from text
--- processing/topic_modelling.py
+++ processing/topic_modelling.py
@@ -8,10 +8,8 @@ import re
 from nltk.corpus import stopwords, words
 from nltk.tokenize import word_tokenize
 from gensim import corpora, models
-import nltk
-
-nltk.download("punkt_tab")
-nltk.download("words")
+import polars as pl
+from hdbscan import HDBSCAN
 
 # %%
 # Database setup
@@ -28,18 +26,6 @@ conn.close()
 
 # %% Text preprocessing
 docs = [row[1] for row in rows]
-""" 
-docs = [doc for doc in docs if doc != ""]
-docs = [doc.replace("\n", " ") for doc in docs]
-docs = [doc.lower() for doc in docs]
-docs = [" ".join(doc.split()) for doc in docs]
-docs = [re.sub(r"[^a-z0-9 ]", "", doc) for doc in docs]
-
-# Remove stopwords
-stop_words = set(stopwords.words("english"))
-docs = [
-    " ".join([word for word in doc.split() if word not in stop_words]) for doc in docs
-] """
 
 
 def clean_text(text):
@@ -54,63 +40,39 @@ def clean_text(text):
 
 
 english_words = set(words.words())
+english_words = {x for x in english_words if len(x) > 1}
 stop_words = set(stopwords.words("english"))
-
-docs = [row[1] for row in rows]
 
 docs = [doc for doc in docs if doc != ""]
 docs = [doc.replace("\n", " ") for doc in docs]
 docs = [clean_text(doc) for doc in docs]
+docs = [doc for doc in docs if doc != ""]
 # %% BERTopic
 
+hdbscan_model = HDBSCAN(
+    cluster_selection_epsilon=0.4, metric="euclidean", prediction_data=True
+)
+
 topic_model = BERTopic(
     embedding_model="all-MiniLM-L6-v2",
     representation_model=KeyBERTInspired(),
-    min_topic_size=10,
-    top_n_words=5,
+    top_n_words=10,
+    hdbscan_model=hdbscan_model,
 )
 topics, probs = topic_model.fit_transform(docs)
+topics_df = pl.DataFrame({"topic": topics, "document": docs})
 
 topic_model.get_topic_info()
-# %%
-# BERTopic Zero Shot
-
-candidate_labels = [
-    "Technology and Software Development",
-    "Cybersecurity",
-    "Artificial Intelligence and Machine Learning",
-    "Startups and Entrepreneurship",
-    "Science and Research",
-    "Economics and Finance",
-    "Politics and Society",
-]
-
-topic_model = BERTopic(
-    embedding_model="all-MiniLM-L6-v2",
-    min_topic_size=10,
-    zeroshot_topic_list=candidate_labels,
-    zeroshot_min_similarity=0.4,
-    representation_model=KeyBERTInspired(),
-    top_n_words=5,
-)
-topics, _ = topic_model.fit_transform(docs)
-# %%
-topic_model.get_topic_info()
-
-# %%
-topic_model.get_document_info(docs)
 
 # %%
 
 # LDA
-
-docs = [word_tokenize(doc) for doc in docs]
-
-dictionary = corpora.Dictionary(docs)
-corpus = [dictionary.doc2bow(doc) for doc in docs]
+tokenized_docs = [word_tokenize(doc) for doc in docs]
+dictionary = corpora.Dictionary(tokenized_docs)
+corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
 
 # %%
-num_topics = 15  # Number of topics to extract
+num_topics = 6  # Number of topics to extract
 lda_model = models.LdaModel(
     corpus, num_topics=num_topics, id2word=dictionary, passes=15
 )
